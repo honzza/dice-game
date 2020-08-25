4359e6093191582080fc70b8cff504846d5b02dc
honzza dvorak <honzzadvorak@gmail.com>
2020-08-25 13:44:13 +0200

    Dots and dice final design

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
     const diceField = document.querySelector('.dice-field')
-    let diceAmount = 6
-    let diceArray = []
+	let diceAmount = 6
+//    let diceArray = []
 
     function createDice() {
         for(let i = 0; i < diceAmount; i++) {
-            const dice = document.createElement('div')
-            dice.setAttribute('id', i)
-            diceField.appendChild(dice)
-            diceArray.push(dice)
-
-            const dot = document.createElement('span')
-            dot.setAttribute('id', 1)
-            dice.appendChild(dot)
+            const die = document.createElement('div')
+            die.setAttribute('id', i)
+			die.classList.add('die')
+            diceField.appendChild(die)
+  //          diceArray.push(die)
+			
+			for(let i = 0; i < 9; i++) {
+				const dot = document.createElement('div')
+				dot.setAttribute('id', i)
+				dot.classList.add('dot')
+				die.appendChild(dot)
+			}
             //<span id="2" style="visibility:hidden"></span>
         }
     }
