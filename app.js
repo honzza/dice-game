a43f32dcfe1dcef8dcacdf2d1b95fcdb84e9bfee
honzza <honzzadvorak@gmail.com>
2020-08-25 22:11:22 +0200

    roll button css

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const diceField = document.querySelector('.dice-field')
+    const rollButton = document.getElementById('roll')
+    rollButton.addEventListener('click', () => {
+        console.log(rollDice())
+    })
 	let diceAmount = 6
 //    let diceArray = []
+
 
     function createDice() {
         for(let i = 0; i < diceAmount; i++) {
@@ -18,7 +23,25 @@ document.addEventListener('DOMContentLoaded', () => {
 				die.appendChild(dot)
 			}
             //<span id="2" style="visibility:hidden"></span>
+            // const dotDefinition = {
+            //     one:[0,1,2,3,5,6,7,8],
+            //     two:[0,1,3,4,5,7,8],
+            //     three:[0,1,3,5,7,8],
+            //     four:[1,3,4,5,7],
+            //     five:[1,3,5,7],
+            //     six:[1,4,7],
+            //     }
         }
+    }
+
+
+    function rollDice() {
+        let diceValues = []
+        let avalValues = ['one', 'two', 'three', 'four', 'five', 'six']
+        for(let i = 0; i < diceAmount; i++) {
+            diceValues[i] = avalValues[Math.floor(Math.random() * avalValues.length)];
+        }
+        return diceValues
     }
 
     createDice()
