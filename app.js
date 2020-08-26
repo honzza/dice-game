1fce387af8cb365199f08937dc9d977ddbb5c02e
honzza dvorak <honzzadvorak@gmail.com>
2020-08-26 13:39:46 +0200

    Functions: roll dice, update dice

--- app.js
+++ app.js
@@ -2,37 +2,56 @@ document.addEventListener('DOMContentLoaded', () => {
     const diceField = document.querySelector('.dice-field')
     const rollButton = document.getElementById('roll')
     rollButton.addEventListener('click', () => {
-        console.log(rollDice())
+        updateDice(rollDice())
     })
 	let diceAmount = 6
-//    let diceArray = []
+	const dotDefinition = {
+			one:[0,1,2,3,5,6,7,8],
+			two:[0,1,3,4,5,7,8],
+            three:[0,1,3,5,7,8],
+            four:[1,3,4,5,7],
+            five:[1,3,5,7],
+            six:[1,4,7],
+    }
 
 
-    function createDice() {
+    function createDice(diceValues = ['one', 'two', 'three', 'four', 'five', 'six']) {
         for(let i = 0; i < diceAmount; i++) {
             const die = document.createElement('div')
             die.setAttribute('id', i)
 			die.classList.add('die')
             diceField.appendChild(die)
-  //          diceArray.push(die)
-			
-			for(let i = 0; i < 9; i++) {
+			let dotsDieDefinition = dotDefinition[diceValues[i]]
+						
+			for(let j = 0; j < 9; j++) {
 				const dot = document.createElement('div')
-				dot.setAttribute('id', i)
+				dot.setAttribute('id', (i.toString() + j.toString()))
 				dot.classList.add('dot')
+				if(dotsDieDefinition.includes(j)) {
+					dot.setAttribute('style', 'visibility:hidden')
+				}
 				die.appendChild(dot)
 			}
-            //<span id="2" style="visibility:hidden"></span>
-            // const dotDefinition = {
-            //     one:[0,1,2,3,5,6,7,8],
-            //     two:[0,1,3,4,5,7,8],
-            //     three:[0,1,3,5,7,8],
-            //     four:[1,3,4,5,7],
-            //     five:[1,3,5,7],
-            //     six:[1,4,7],
-            //     }
         }
     }
+
+
+	function updateDice(diceValues = ['one', 'two', 'three', 'four', 'five', 'six']) {
+        for(let i = 0; i < diceAmount; i++) {
+            const die = document.getElementById(i)
+            let dotsDieDefinition = dotDefinition[diceValues[i]]
+						
+			for(let j = 0; j < 9; j++) {
+				const dot = document.getElementById(i.toString() + j.toString())
+				if(dotsDieDefinition.includes(j)) {
+					dot.style = 'visibility:hidden'
+				}
+				else {
+					dot.removeAttribute('style')
+				}
+			}
+        }
+	}
 
 
     function rollDice() {
