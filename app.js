423a609388cb7fd7c166b32af9a4dcfddabe25cf
honzza dvorak <honzzadvorak@gmail.com>
2020-08-28 15:19:39 +0200

    major refactoring
    getting ready for score calculation

--- app.js
+++ app.js
@@ -2,68 +2,64 @@ document.addEventListener('DOMContentLoaded', () => {
     const diceField = document.querySelector('.dice-field')
     const rollButton = document.getElementById('roll')
     rollButton.addEventListener('click', () => {
-        updateDice(rollDice())
+        updateDice(diceDef)
     })
-	let diceAmount = 6
-	const dotDefinition = {
-			one:[0,1,2,3,5,6,7,8],
-			two:[0,1,3,4,5,7,8],
-            three:[0,1,3,5,7,8],
-            four:[1,3,4,5,7],
-            five:[1,3,5,7],
-            six:[1,4,7],
-    }
+	const dotDef = [
+			[0,1,2,3,5,6,7,8],
+			[0,1,3,4,5,7,8],
+            [0,1,3,5,7,8],
+            [1,3,4,5,7],
+            [1,3,5,7],
+            [1,4,7],
+    ]
+	let diceDef = [1, 2, 3, 4, 5, 6]
 
-
-    function createDice(diceValues = ['one', 'two', 'three', 'four', 'five', 'six']) {
-        for(let i = 0; i < diceAmount; i++) {
+    function createDice(diceDef) {
+		for(let i = 0; i < diceDef.length; i++) {
             const die = document.createElement('div')
             die.setAttribute('id', i)
 			die.classList.add('die')
             diceField.appendChild(die)
-			let dotsDieDefinition = dotDefinition[diceValues[i]]
+			let dotsDie = dotDef[diceDef[i]-1]
 			die.addEventListener('click', () => {
-				console.log(die.id)
 				die.classList.contains('selected') ? die.classList.remove('selected') : die.classList.add('selected')
-			})
-						
+			})	
 			for(let j = 0; j < 9; j++) {
 				const dot = document.createElement('div')
 				dot.setAttribute('id', (i.toString() + j.toString()))
 				dot.classList.add('dot')
-				if(dotsDieDefinition.includes(j)) {
-					dot.setAttribute('style', 'visibility:hidden')
-				}
+				if(dotsDie.includes(j)) dot.setAttribute('style', 'visibility:hidden')
 				die.appendChild(dot)
 			}
         }
     }
 
-
-	function updateDice(diceValues = ['one', 'two', 'three', 'four', 'five', 'six']) {
-        for(let i = 0; i < diceAmount; i++) {
+	function updateDice(diceDef) {
+        for(let i = 0; i < diceDef.length; i++) {
             const die = document.getElementById(i)
-            let dotsDieDefinition = dotDefinition[diceValues[i]]
-						
+			if(die.classList.contains('selected')) continue
+            diceDef[i] = Math.floor(Math.random() * 6) + 1
+			let dotsDie = dotDef[diceDef[i]-1]
 			for(let j = 0; j < 9; j++) {
 				const dot = document.getElementById(i.toString() + j.toString())
-				dotsDieDefinition.includes(j) ? dot.style = 'visibility:hidden' : dot.removeAttribute('style')
+				dotsDie.includes(j) ? dot.style = 'visibility:hidden' : dot.removeAttribute('style')
 			}
         }
+		console.log(diceDef)
+		return diceDef
 	}
 
+    createDice(diceDef)
+	updateDice(diceDef)
+	
+	let pokus = {
+	0: [1, 'unselected'],
+	1: [2, 'unselected']
+	}
 
-    function rollDice() {
-        let diceValues = []
-        let avalValues = ['one', 'two', 'three', 'four', 'five', 'six']
-        for(let i = 0; i < diceAmount; i++) {
-            diceValues[i] = avalValues[Math.floor(Math.random() * avalValues.length)];
-        }
-        return diceValues
-    }
-
-
-    createDice()
+	console.log(pokus[0][1])
+	console.log(Object.keys(pokus).length)
+	
 })
 
 //11:28
